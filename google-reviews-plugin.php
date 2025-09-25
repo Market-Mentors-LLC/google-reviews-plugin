8d17364a0309fa1367506b8ef4bf65f5f6d5df10
Tyler Seabury <tylerseabury@gmail.com>
2025-09-25 13:45:58 -0400

    removed useless comments

--- google-reviews-plugin.php
+++ google-reviews-plugin.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Google Reviews Plugin
  * Plugin URI:        https://marketmentors.com
  * Description:       Uses the Google Places API to display the latest reviews.
- * Version:           0.0.2
+ * Version:           0.0.3
  * Author:            Market Mentors, LLC
  * Author URI:        https://marketmentors.com
  * Text Domain:       google-reviews-plugin
@@ -54,13 +54,11 @@ use MarketMentors\GoogleReviewsPlugin\src\UpdaterConfig;
 use MarketMentors\GoogleReviewsPlugin\src\Activator;
 use MarketMentors\GoogleReviewsPlugin\src\Deactivator;
 
-// Prevent direct access
 if (! defined('ABSPATH')) {
   exit;
 }
 
-// Define plugin constants
-define('GOOGLE_REVIEWS_PLUGIN_VERSION', '0.0.2');
+define('GOOGLE_REVIEWS_PLUGIN_VERSION', '0.0.3');
 define('GOOGLE_REVIEWS_PLUGIN_FILE', __FILE__);
 define('GOOGLE_REVIEWS_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('GOOGLE_REVIEWS_PLUGIN_URL', plugin_dir_url(__FILE__));
@@ -68,9 +66,6 @@ define('GOOGLE_REVIEWS_PLUGIN_BASENAME', plugin_basename(__FILE__));
 define('MIN_PHP_VERSION', '8.2');
 define('MIN_WP_VERSION', '6.8.0');
 
-/**
- * Ensure dependencies are loaded
- */
 if (!file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
   throw new \Error(
     'Autoloader not found. You must run <code>composer install</code> from the plugin directory.',
@@ -78,54 +73,30 @@ if (!file_exists($composer = __DIR__ . '/vendor/autoload.php')) {
 }
 require_once $composer;
 
-/**
- * Ensure compatible version of PHP is used
- */
 if (version_compare(MIN_PHP_VERSION, phpversion(), '>=')) {
   throw new \Error(
     "Invalid PHP version: You must be using PHP " . MIN_PHP_VERSION . " or greater.",
   );
 }
 
-/**
- * Ensure compatible version of WordPress is used
- */
 if (version_compare(MIN_WP_VERSION, \get_bloginfo('version'), '>=')) {
   throw new \Error(
     "Invalid WordPress version: You must be using WordPress " . MIN_WP_VERSION . " or greater.",
   );
 }
 
-/**
- * The code that runs during plugin activation.
- */
 \register_activation_hook(__FILE__, [Activator::class, 'activate']);
 
-/**
- * The code that runs during plugin deactivation.
- */
 \register_deactivation_hook(__FILE__, [Deactivator::class, 'deactivate']);
 
-
-
-/**
- * Main plugin class
- */
 class Main
 {
 
   /**
-   * Plugin instance
-   *
    * @var Main
    */
   private static $instance = null;
 
-  /**
-   * Get plugin instance
-   *
-   * @return Main
-   */
   public static function instance()
   {
     if (null === self::$instance) {
@@ -134,21 +105,8 @@ class Main
     return self::$instance;
   }
 
-  /**
-   * Begins execution of the plugin.
-   *
-   * Since everything within the plugin is registered via hooks,
-   * then kicking off the plugin from this point in the file does
-   * not affect the page life cycle.
-   *
-   * @since    0.0.1
-   */
   private function __construct()
   {
-    /**
-     * The core plugin class that is used to define internationalization,
-     * admin-specific hooks, and public-facing site hooks.
-     */
     $plugin = new GoogleReviewsPlugin(
       new UpdaterConfig(
         metadataUrl: 'https://github.com/Market-Mentors-LLC/google-reviews-plugin',
--- src/GoogleReviewsPlugin.php
+++ src/GoogleReviewsPlugin.php
@@ -3,83 +3,25 @@
 namespace MarketMentors\GoogleReviewsPlugin\src;
 
 use \YahnisElsts\PluginUpdateChecker\v5\PucFactory;
-use \YahnisElsts\PluginUpdateChecker\v5\PluginUpdateChecker;
 use MarketMentors\GoogleReviewsPlugin\src\admin\AdminController;
 use MarketMentors\GoogleReviewsPlugin\src\public\PublicController;
 use MarketMentors\GoogleReviewsPlugin\src\integration\shortcode\LatestReviewsCard;
 
 /**
- * The file that defines the core plugin class
- *
- * A class definition that includes attributes and functions used across both the
- * public-facing side of the site and the admin area.
- *
- * @link       https://marketmentors.com
- * @since      0.0.1
- *
- */
-
-/**
- * The core plugin class.
- *
- * This is used to define internationalization, admin-specific hooks, and
- * public-facing site hooks.
- *
- * Also maintains the unique identifier of this plugin as well as the current
- * version of the plugin.
- *
  * @since      0.0.1
  * @author     Market Mentors, LLC. <accounts@marketmentors.com>
  */
 class GoogleReviewsPlugin
 {
 
-  /**
-   * The loader that's responsible for maintaining and registering all hooks that power
-   * the plugin.
-   *
-   * @since    0.0.1
-   * @access   protected
-   * @var      Loader    $loader    Maintains and registers all hooks for the plugin.
-   */
   protected $loader;
 
-  /**
-   * The unique identifier of this plugin.
-   *
-   * @since    0.0.1
-   * @access   protected
-   * @var      string    $plugin_name    The string used to uniquely identify this plugin.
-   */
   protected $plugin_name;
 
-  /**
-   * The current version of the plugin.
-   *
-   * @since    0.0.1
-   * @access   protected
-   * @var      string    $version    The current version of the plugin.
-   */
   protected $version;
 
-  /**
-   * The updater.
-   * 
-   * @since 0.0.1
-   * 
-   * @var PluginUpdateChecker
-   */
   private $updater;
 
-  /**
-   * Define the core functionality of the plugin.
-   *
-   * Set the plugin name and the plugin version that can be used throughout the plugin.
-   * Load the dependencies, define the locale, and set the hooks for the admin area and
-   * the public-facing side of the site.
-   *
-   * @since    0.0.1
-   */
   public function __construct(
     UpdaterConfig $updaterConfig,
   ) {
@@ -106,13 +48,6 @@ class GoogleReviewsPlugin
     $this->define_public_hooks();
   }
 
-  /**
-   * Register all of the hooks related to the admin area functionality
-   * of the plugin.
-   *
-   * @since    0.0.1
-   * @access   private
-   */
   private function define_admin_hooks()
   {
 
@@ -122,13 +57,7 @@ class GoogleReviewsPlugin
     $this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
   }
 
-  /**
-   * Register all of the hooks related to the public-facing functionality
-   * of the plugin.
-   *
-   * @since    0.0.1
-   * @access   private
-   */
+
   private function define_public_hooks()
   {
 
@@ -138,11 +67,6 @@ class GoogleReviewsPlugin
     $this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
   }
 
-  /**
-   * Run the loader to execute all of the hooks with WordPress.
-   *
-   * @since    0.0.1
-   */
   public function run()
   {
     $this->loader->run();
@@ -150,35 +74,16 @@ class GoogleReviewsPlugin
     new LatestReviewsCard();
   }
 
-  /**
-   * The name of the plugin used to uniquely identify it within the context of
-   * WordPress and to define internationalization functionality.
-   *
-   * @since     0.0.1
-   * @return    string    The name of the plugin.
-   */
   public function get_plugin_name()
   {
     return $this->plugin_name;
   }
 
-  /**
-   * The reference to the class that orchestrates the hooks with the plugin.
-   *
-   * @since     0.0.1
-   * @return    Loader    Orchestrates the hooks of the plugin.
-   */
   public function get_loader()
   {
     return $this->loader;
   }
 
-  /**
-   * Retrieve the version number of the plugin.
-   *
-   * @since     0.0.1
-   * @return    string    The version number of the plugin.
-   */
   public function get_version()
   {
     return $this->version;
